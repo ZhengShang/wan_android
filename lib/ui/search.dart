b972651aea18d545a0e0da3feabb892608bfb602
zheng100 <wolfer>
2018-08-30 23:45:03 +0800

    update search

--- lib/ui/search.dart
+++ lib/ui/search.dart
@@ -5,21 +5,34 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: MediaQuery.of(context).padding,
-          child: Column(
+          body: IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 14.0),
+          margin: MediaQuery.of(context).padding,
+          child: Row(
             children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    icon: Icon(Icons.search),
-                    hintText: 'Please enter a search term'),
+              Flexible(
+                child: Container(
+                  margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  padding: EdgeInsets.only(left: 8.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      color: Colors.black26),
+                  child: TextField(
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(Icons.search),
+                        hintText: "关键词"),
+                  ),
+                ),
               ),
-              MyChipGroup()
+              Text("Cancel")
             ],
           ),
-        ),
-      ),
+        ), //new
+      )),
     );
   }
 }
