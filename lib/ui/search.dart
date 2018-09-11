139cbaa8fcdb6d29c699b72f8485960d333ae294
ShangZheng <shang10023@126.com>
2018-09-11 17:51:08 +0800

    complete half search

--- lib/ui/search.dart
+++ lib/ui/search.dart
@@ -2,8 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:http/http.dart' as http;
+import 'package:wan_android/model/home_page_json.dart';
 import 'package:wan_android/model/hotkey_json.dart';
+import 'package:wan_android/widget/tag_widget.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -13,7 +16,10 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _textController =
       new TextEditingController(); //new
+  final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
+  final TextStyle _grayText = new TextStyle(fontSize: 12.0, color: Colors.grey);
   String _hotKeys = "";
+  List<Article> _listData = [];
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +27,6 @@ class _SearchPageState extends State<SearchPage> {
       statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
       statusBarIconBrightness: Brightness.dark,
     ));
-    print('Main part');
     return MaterialApp(
       home: Scaffold(
           body: IconTheme(
@@ -49,6 +54,7 @@ class _SearchPageState extends State<SearchPage> {
                           print('The hotkey is $text');
                           setState(() {
                             _hotKeys = text;
+                            doSearch();
                           });
                         },
                         decoration: InputDecoration(
@@ -89,20 +95,155 @@ class _SearchPageState extends State<SearchPage> {
             TextSelection.collapsed(offset: keyword.length);
         setState(() {
           _hotKeys = keyword;
+          doSearch();
         });
       });
     } else {
       return Flexible(
           child: ListView.builder(
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(index.toString()),
-            selected: true,
-          );
+          return _buildRow(_listData[index]);
         },
-        itemCount: 10,
+        itemCount: _listData.length,
       ));
     }
+  }
+
+  Widget _buildRow(Article article) {
+    return Card(
+      margin: EdgeInsets.only(left: 12.0, top: 6.0, right: 12.0, bottom: 6.0),
+      elevation: 12.0,
+      child: InkWell(
+        onTap: () {
+          goDetailPage(article.link, article.collect);
+        },
+        child: Padding(
+          padding: EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      print('Go to the author page');
+                    },
+                    child: Text(
+                      article.author,
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    article.niceDate,
+                    style: _grayText,
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              Text(
+                article.title,
+                style: _biggerFont,
+              ),
+              TagWidget(article.tags),
+              Text(article.desc, style: _grayText),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () {
+                          print('Go to superChapter page');
+                        },
+                        child: Text(
+                          article.superChapterName,
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_right,
+                        color: Colors.grey,
+                      ),
+                      InkWell(
+                          onTap: () {
+                            print('Go to chapter page');
+                          },
+                          child: Text(article.chapterName,
+                              style: TextStyle(color: Colors.blue)))
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(
+                        article.collect
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: article.collect ? Colors.red : Colors.grey),
+                    onPressed: () {
+                      print('do collect');
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  doSearch() async {
+    _listData.clear();
+    Map<String, String> map = {'k': _hotKeys};
+    final respone = await http
+        .post('http://www.wanandroid.com/article/query/0/json', body: map)
+        .timeout(Duration(seconds: 5));
+    print('End search , and  body is ${respone.body}');
+    var homePageJson = HomePageJson.fromJson(json.decode(respone.body));
+    if (homePageJson.errorCode >= 0) {
+      setState(() {
+        _listData = homePageJson.data.datas;
+      });
+      FocusScope.of(context).requestFocus(new FocusNode());
+    }
+  }
+
+  goDetailPage(String url, bool isCollect) {
+    print('Go detail page');
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => WebviewScaffold(
+                  url: url,
+                  appBar: AppBar(
+//                          title: Text(article.title),
+                    centerTitle: true,
+                    actions: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          isCollect ? Icons.favorite : Icons.favorite_border,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          print('Do collect');
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          print('Do share');
+                        },
+                      )
+                    ],
+                  ),
+                  scrollBar: true,
+                  withLocalStorage: true,
+                )));
   }
 }
 
